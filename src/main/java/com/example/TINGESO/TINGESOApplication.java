f247463ac16cd06d72ffb39309138903e744fafc
Raul <raul.gonzalez.v@usach.cl>
2026-04-03 02:20:06 -0300

    Renamed application

--- src/main/java/com/example/TINGESO/TINGESOApplication.java
+++ src/main/java/com/example/TINGESO/TINGESOApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.TINGESO;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TINGESOApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TINGESOApplication.class, args);
 	}
 
 }
